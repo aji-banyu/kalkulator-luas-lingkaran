a4a3dc597ab2223206d1ddcbcdfc29126473f974
aji-banyu <ajibanyurizqi@gmail.com>
2024-10-11 19:28:44 +0700

    tambah fitur reset

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 console.log("hellooooo");
 
 const input = document.getElementById("radius");
-const button = document.querySelector("button");
 const hasil = document.querySelector("#hasil");
-button.addEventListener("click", () => {
+const btnHitung = document.querySelector("#hitung");
+const btnReset = document.querySelector("#reset");
+
+// fungsi tombol hitung
+btnHitung.addEventListener("click", () => {
   const inputValue = input.value;
   const hasilAkhir = 3.14 * (inputValue * 2);
   hasil.innerHTML = `2 x π x r = 2 x 3,14 x ${inputValue} = ${hasilAkhir}`;
+});
+
+// funsi tombol reset
+btnReset.addEventListener("click", () => {
+  input.value = "";
+  hasil.innerHTML = "";
 });
